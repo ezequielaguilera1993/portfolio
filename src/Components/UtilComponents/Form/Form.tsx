b1d812ba9e3de312ed1422dcdbd6d864533d304f
ezequielaguilera1993 <ezequielaguilera1993@gmail.com>
2021-08-16 14:08:19 -0300

    Nota

--- src/Components/UtilComponents/Form/Form.tsx
+++ src/Components/UtilComponents/Form/Form.tsx
@@ -2,16 +2,29 @@ import { useForm } from "react-hook-form";
 
 import Style from "./Form.module.scss";
 import emailjs from 'emailjs-com'
+import { useEffect } from "react";
 
 interface IinformationObject {
     name: string; mail: string; phone: string; message: string;
 };
 
-
 function sendMail(informationObject: { name: string, mail: string, phone: string, message: string }) {
     emailjs.send("gmail", "template_2tshsrs", informationObject, "user_1aZLUJ4EJdglFQR5mfcZ1")
         .then(() => alert(JSON.stringify(informationObject)))
         .catch(() => alert("error"))
+}
+
+function cartJumpsCounter(id: string, cb: Function) {
+    let element: any = document.getElementById(id)
+    element?.addEventListener("keydown", (value: any) => {
+        if (element) {
+            cb(element)
+            let { value, clientHeight, offsetHeight, scrollHeight, textLength, selectionEnd,
+                selectionStart } = element
+            console.log(value, clientHeight, offsetHeight, scrollHeight, textLength, selectionEnd, selectionStart);
+
+        }
+    })
 }
 
 export function Form() {
@@ -26,6 +39,11 @@ export function Form() {
         sendMail(data)
     };
 
+    useEffect(() => {
+        cartJumpsCounter(Style.divEditable, (a: string) => console.dir(a))
+    })
+
+    console.log('Aca!')
     return (
         <div id={Style.Container} >
             {/* <form>
@@ -36,28 +54,28 @@ export function Form() {
             </form> */}
 
 
-            <img src="https://i.imgur.com/jbw9aC3.jpg" id={Style.backgroundImage} />
+            <img src="https://i.imgur.com/Dz4ynCf.jpg" id={Style.backgroundImage} />
 
             <form onSubmit={handleSubmit(onSubmit)} autoComplete="on">
-                <div id={Style.name}>
-                    <label htmlFor="name">Nombre</label>
+                <div id={Style.name} className={Style.inputContainer}>
+                    <label htmlFor="name">Nombre: </label>
                     <input autoComplete="on"
-                        placeholder='"Florencia"'
+                        placeholder='Florencia'
                         {...register("name", { required: false })}
+                        type="text"
                     />
                     {errors.name && <p>This is required</p>}
                 </div>
 
-                {/* <div>
+                <div id={Style.mail} className={Style.inputContainer}>
                     <label htmlFor="mail" placeholder='"florencia1990@hotmail.com"'>
-                        Email
-                    </label>
-                    <input autoComplete="on"  {...register("mail", { required: true })} type="mail" placeholder='"florencia1990@hotmail.com"' />
+                        Email: </label>
+                    <input autoComplete="on"  {...register("mail", { required: true })} type="mail" placeholder='florencia1990@hotmail.com' />
                     {errors.mail && <p>Campo requerido para poder contactarme</p>}
                 </div>
 
-                <div>
-                    <label htmlFor="phone">Telefono</label>
+                <div id={Style.phone} className={Style.inputContainer}>
+                    <label htmlFor="phone">Teléfono: </label>
                     <input autoComplete="on"
                         {...register("phone", { required: false })} type="tel"
                         placeholder='"+541128676833"'
@@ -65,17 +83,19 @@ export function Form() {
                     {errors.phone && <p>Campo requerido para poder contactarme</p>}
                 </div>
 
-                <div>
-                    <label htmlFor="message">Nota</label>
-                    <div contentEditable={true}
+                <div id={Style.message} className={Style.inputContainer}>
+                    <label htmlFor="message">Nota: </label>
+                    <textarea id={Style.divEditable} contentEditable={true}
                         placeholder='[...]'
                         {...register("message", { required: false })}
-                        style={{ height: "100px", textAlign: "start", borderRadius: "80px" }}
-                    ></div>
-
+                        maxLength={300}
+                        rows={8}
+                    >
+                    </textarea>
                     {errors.message && <p>Campo requerido para poder contactarme</p>}
                 </div>
-                <input type="submit" /> */}
+
+                {/* <input type="submit" /> */}
             </form>
 
         </div>
