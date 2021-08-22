9e37ec39439f0cd90fe7d072c45ce193134a6c81
ezequielaguilera1993 <ezequielaguilera1993@gmail.com>
2021-08-22 05:21:56 -0300

    Mejorado boton de lanzamiento de confetti

--- src/Components/Content/Portfolio/Portfolio.tsx
+++ src/Components/Content/Portfolio/Portfolio.tsx
@@ -162,11 +162,13 @@ export const Portfolio: React.FunctionComponent<{}> = (conf) => {
                         <button id={Style.moreConfetti} onClick={throwMoreConfetti}></button>
                         :
                         showMoreConfetti && moreConfettiLoading ?
+
                             <svg id={Style.moreConfettiLoading} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" style={{ margin: "auto", background: "none", display: "block", shapeRendering: "auto" }} width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
                                 <circle cx="50" cy="50" r="32" stroke-width="8" stroke="#4877be" stroke-dasharray="50.26548245743669 50.26548245743669" fill="none" stroke-linecap="round">
                                     <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="1s" keyTimes="0;1" values="0 50 50;360 50 50"></animateTransform>
                                 </circle>
                             </svg>
+
                             :
                             null
                 }
